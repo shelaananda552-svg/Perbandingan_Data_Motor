846033a963c1fa8105eac407e5f70d6711f7c951
shelaananda552-svg <shelaananda552@gmail.com>
2026-06-25 03:04:59 +0700

    Add files via upload

--- java.script.js
+++ java.script.js
@@ -2,20 +2,54 @@ const searchInput = document.getElementById("searchInput");
 
 searchInput.addEventListener("keyup", function(){
 
-let filter = searchInput.value.toLowerCase();
+const keyword = searchInput.value.toLowerCase();
 
-let rows = document.querySelectorAll("#motorTable tbody tr");
+const rows =
+document.querySelectorAll("#motorTable tbody tr");
 
 rows.forEach(row => {
 
-let text = row.innerText.toLowerCase();
+const text = row.innerText.toLowerCase();
 
-if(text.includes(filter)){
+if(text.includes(keyword)){
 row.style.display = "";
-}else{
+}
+else{
 row.style.display = "none";
 }
 
 });
 
 });
+
+function sortHarga(){
+
+const tbody =
+document.querySelector("#motorTable tbody");
+
+const rows =
+Array.from(tbody.querySelectorAll("tr"));
+
+rows.sort((a,b)=>{
+
+const hargaA =
+parseInt(
+a.children[2].dataset.harga
+);
+
+const hargaB =
+parseInt(
+b.children[2].dataset.harga
+);
+
+return hargaA - hargaB;
+
+});
+
+tbody.innerHTML="";
+
+rows.forEach(row=>{
+tbody.appendChild(row);
+});
+
+}
